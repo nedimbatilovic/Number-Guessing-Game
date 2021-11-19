0c199b180d45e42c574a93d82cea8741739adf72
nedimbatilovic <nedimco2006@gmail.com>
2021-11-19 23:30:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random _Random = new Random();
+            int Num = _Random.Next(1, 10);
+            int Guess = 0;
+            Console.Write("num: ");
+
+            while (true)
+            {
+                Guess = Convert.ToInt32(Console.ReadLine());
+                if (Guess < Num)
+                {
+                    Console.WriteLine("num low");
+                } else if (Guess > Num)
+                {
+                    Console.WriteLine("num high");
+                } else
+                {
+                    Console.WriteLine("correct");
+                    Console.Write("continue y/n: ");
+                    string Choice = Console.ReadLine();
+                    if (Choice == "y") { continue; } else { break; }
+                }
+            }
+        }
+    }
+}
