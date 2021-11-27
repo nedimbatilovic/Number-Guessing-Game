956b18787c41dd01cc65755e8756fc33c17def33
nedimbatilovic <nedimco2006@gmail.com>
2021-11-27 11:57:30 +0100

    small fixes

--- Program.cs
+++ Program.cs
@@ -1,31 +1,51 @@
-﻿namespace Number_Guessing_Game
+﻿using System;
+
+namespace Number_Guessing_Game
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Random _Random = new Random();
-            int Num = _Random.Next(1, 10);
-            int Guess = 0;         
+            Program _program = new Program();
+            _program.NumGuess();
+        }
 
+        public void NumGuess()
+        {
+            Random _Random = new();
+            int Num = _Random.Next(1, 1000);
             while (true)
             {
                 Console.Write("num: ");
-                Guess = Convert.ToInt32(Console.ReadLine());
-                if (Guess < Num)
-                {
-                    Console.WriteLine("num low");
-                } else if (Guess > Num)
-                {
-                    Console.WriteLine("num high");
+                int Guess;
+                bool resume = int.TryParse(Console.ReadLine(), out Guess);
+                if (resume)
+                {                        
+                    if (Guess < Num)
+                    {
+                        Console.WriteLine($"{Guess} is too low.");
+                    }
+                    else if (Guess > Num)
+                    {
+                        Console.WriteLine($"{Guess} is too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Guess} is correct.{Environment.NewLine}");
+                        Num = _Random.Next(1, 1000);
+                        Console.Write("continue y/n: ");
+                        string Choice = Console.ReadLine();
+                        if (Choice == "n")
+                        {
+                            break;
+                        }
+                    }        
                 } else
                 {
-                    Console.WriteLine("correct");
-                    Console.Write("continue y/n: ");
-                    string Choice = Console.ReadLine();
-                    if (Choice == "n") { break; }
+                    Console.WriteLine("Guess must be an integer.");
                 }
             }
         }
     }
 }
+
